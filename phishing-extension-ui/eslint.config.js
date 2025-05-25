e6140088774a8f77006c8d5506e0ed1cf8d20bc1
Rohit <pandarohit05@gmail.com>
2025-05-25 15:08:31 +0800

    Fix ESLint false positive on App JSX usage

--- phishing-extension-ui/eslint.config.js
+++ phishing-extension-ui/eslint.config.js
@@ -2,16 +2,28 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import babelParser from "@babel/eslint-parser";
 
 export default defineConfig([
-  js.configs.recommended, // Base JS rules
+  js.configs.recommended,
 
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
+      parser: babelParser,
       ecmaVersion: 2021,
       sourceType: "module",
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+        sourceType: "module",
+      },
     },
     plugins: {
       react: pluginReact,
@@ -24,6 +36,7 @@ export default defineConfig([
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/jsx-no-target-blank": ["error", { allowReferrer: false }],
+      "no-unused-vars": "off",
     },
   },
 ]);
